66523e76de03d4ab0acdef61da615283a426134e
Parbhat-Bansal <parbhat.bansal001@gmail.com>
2024-09-13 21:45:07 +0530

    sept 13

---
+++ Basic/ListNode/Que148.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+         if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = getMiddle(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return merge(left, right);
+    }
+
+    public ListNode getMiddle(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+
+    public ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                tail.next = left;
+                left = left.next;
+            } else {
+                tail.next = right;
+                right = right.next;
+            }
+            tail = tail.next;
+        }
+        
+        if (left != null) {
+            tail.next = left;
+        } else {
+            tail.next = right;
+        }
+        
+        return dummy.next;
+    }
+    
+    
+}
